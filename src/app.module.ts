31e994e626444e025f334138a7b03e7203132ff9
Allan Melo <allanmelo@AmeMacBook.local>
2020-10-25 23:46:17 -0300

    Set connection w/ MongoDb Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.DATABASE_URI,
+      { useNewUrlParser: true, 
+        useCreateIndex: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false })
+  ],
   controllers: [],
   providers: [],
 })
